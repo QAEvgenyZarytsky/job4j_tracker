bfd54f6a6f62c9fc283352fb5b28093e310f104f
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-04-30 01:50:51 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Wolf {
+    public void tryEat(Ball ball) {
+        System.out.println("Wolf try eat Ball");
+        ball.tryRun(false);
+    }
+
+    public void eat(Girl girl) {
+    }
+}
