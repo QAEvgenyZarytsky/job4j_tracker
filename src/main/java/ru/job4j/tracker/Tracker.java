71c8541ba234e4a2b63f44478f0f2c03454b3794
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-13 13:23:30 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #543131]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,7 +32,7 @@ public class Tracker {
         if (index == -1) {
             return false;
         }
-        item.setId(items[index].getId());
+        item.setId(id);
         items[index] = item;
         return true;
     }
