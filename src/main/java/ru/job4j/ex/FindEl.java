5bbd684d6b68a69826d0866b1cbc975618ac19c4
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-22 21:56:57 +0300

    0.4. Пользовательские исключения. [#219367 #544155]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,6 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 result = i;
+                break;
             }
         }
         if (result == -1) {
@@ -14,9 +15,28 @@ public class FindEl {
         return result;
     }
 
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         String[] array = new String[]{"one", "two", "three", "four", "five"};
-        String key = "test";
+        String key = "two";
         try {
             int index = indexOf(array, key);
             System.out.println(index);
