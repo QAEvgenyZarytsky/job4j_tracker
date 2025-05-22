ba3496dd36131e0be0f2e12c21ec87c1e6fc2b7d
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-22 22:13:10 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #544215]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -50,6 +50,10 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
 
         if (!hasUpCase) {
