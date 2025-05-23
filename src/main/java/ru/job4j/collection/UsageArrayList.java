a8279b80b9bb97f88ea4930ff714c57946c9cdd0
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-24 00:49:23 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import ru.job4j.tracker.SingleTracker;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Petr");
+        array.add("Ivan");
+        array.add("Stepan");
+        for (String str : array) {
+            System.out.println(str);
+        }
+    }
+}
