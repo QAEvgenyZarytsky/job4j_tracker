c7ef549abb9f83c4a50698e4807c86291a231e03
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-29 01:36:54 +0300

    4. Аттестация [#504878]

---
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -0,0 +1,86 @@
+package ru.job4j.hashmap;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double sumScoreOfSubject = 0;
+        int countOfSubject = 0;
+        for (Pupil pupil : pupils) {
+            Iterator<Subject> iterator = pupil.subjects().iterator();
+            while (iterator.hasNext()) {
+                sumScoreOfSubject += iterator.next().score();
+                countOfSubject++;
+            }
+        }
+        return sumScoreOfSubject / countOfSubject;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> listScorePupil = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sumScoreOfSubject = 0;
+            int countOfSubject = 0;
+            Iterator<Subject> subjectIterator = pupil.subjects().iterator();
+            while (subjectIterator.hasNext()) {
+                sumScoreOfSubject += subjectIterator.next().score();
+                countOfSubject++;
+            }
+            listScorePupil.add(new Label(pupil.name(), sumScoreOfSubject / countOfSubject));
+        }
+        return listScorePupil;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        Map<String, Integer> temp = new LinkedHashMap<>();
+        for (Pupil pupil : pupils) {
+            Iterator<Subject> subjectIterator = pupil.subjects().iterator();
+            while (subjectIterator.hasNext()) {
+                Subject subject = subjectIterator.next();
+                int sumScoreOfSubject = (temp.get(subject.name()) == null)
+                        ? 0 : temp.get(subject.name());
+                sumScoreOfSubject += subject.score();
+                temp.put(subject.name(), sumScoreOfSubject);
+            }
+        }
+        List<Label> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> subject : temp.entrySet()) {
+            result.add(new Label(subject.getKey(), (double) subject.getValue() / pupils.size()));
+        }
+        return result;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> listPupils = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            int sumScore = 0;
+            Iterator<Subject> subjectIterator = pupil.subjects().iterator();
+            while (subjectIterator.hasNext()) {
+                sumScore += subjectIterator.next().score();
+            }
+            listPupils.add(new Label(pupil.name(), sumScore));
+        }
+        listPupils.sort(Comparator.naturalOrder());
+        return listPupils.get(listPupils.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Integer> temp = new LinkedHashMap<>();
+        for (Pupil pupil : pupils) {
+            Iterator<Subject> subjectIterator = pupil.subjects().iterator();
+            while (subjectIterator.hasNext()) {
+                Subject subject = subjectIterator.next();
+                int sumScoreOfSubject = (temp.get(subject.name()) == null)
+                        ? 0 : temp.get(subject.name());
+                sumScoreOfSubject += subject.score();
+                temp.put(subject.name(), sumScoreOfSubject);
+            }
+        }
+        List<Label> listSubject = new ArrayList<>();
+        for (Map.Entry<String, Integer> subject : temp.entrySet()) {
+            listSubject.add(new Label(subject.getKey(), (double) subject.getValue()));
+        }
+        listSubject.sort(Comparator.naturalOrder());
+        return listSubject.get(listSubject.size() - 1);
+    }
+}
