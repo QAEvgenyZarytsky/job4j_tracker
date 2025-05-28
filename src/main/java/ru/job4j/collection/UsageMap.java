293fcc4073d058920722f54ff327c5918f425c2c
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-05-28 23:22:46 +0300

    0. Map, HashMap [#212652 #544794]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,11 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("test@gmail.com", "Ivanov Ivan");
+        map.put("test1@gmail.com", "Neo Matrixx");
+        map.put("test2@gmail.com", "Shrek Oreo");
+        map.put("test3@gmail.com", "Kot Off");
+        map.put("test@gmail.com", "Ivanov Update");
+
         for (Map.Entry<String, String> entry : map.entrySet()) {
             System.out.println("key = " + entry.getKey());
             System.out.println("value = " + entry.getValue());
