50aa2f4a3ac82370505961c573fe268fd7e71691
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-06-02 21:39:59 +0300

    2.1. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("Начало блока \"лямды\"");
+            System.out.println("Используем Integer.compare для сравнения длинны");
+            System.out.println("В return передаём:  Integer.compare(right.length(), left.length());");
+            return Integer.compare(right.length(), left.length());
+        };
+        strings.sort(comparator);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
