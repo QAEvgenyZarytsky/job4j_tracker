8bf3055850980bb0802a04433b494a0280a8d8d4
evgeny.zarutsky <evgeny.zarutsky@gmail.com>
2025-06-03 22:14:15 +0300

    6. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/function/FunctionCalculator.java
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -12,14 +12,4 @@ public class FunctionCalculator {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
-        List<Double> result1 = function.diapason(5, 8, x -> 2 * x * x + 3 * x + 1);
-        List<Double> result2 = function.diapason(5, 8, x -> Math.pow(3, x));
-        int i = 0;
-//        List<Double> expected = Arrays.asList(11D, 13D, 15D);
-
-    }
 }
